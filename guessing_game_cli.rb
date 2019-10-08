f4a3c2ef62086711051ac8581bf98d4c148f8433
Jake Swogger <jakeswogger1@gmail.com>
2019-10-08 20:01:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,10 +2,9 @@ def run_guessing_game
   random_num = rand(6) + 1 
   puts "Please guess a number between 1-6"
   user_num = gets 
-  user_num
   if user_num == random_num.to_s
     puts "You guessed the correct number!"
-    elsif user_num == random_num
+    elsif user_num == random_num.to_s
     puts "Sorry! The computer guessed #{random_num}"
     elsif user_num == "exit"
     puts "Goodbye!"
